c7205fe0ba841a50a174af28681b0f5ac4bafae3
Sebastian Kümper <skuemper@gmx.de>
2021-10-10 00:12:23 +0200

    add some placeholder buttons for object selection

--- Scenes/ui.gd
+++ Scenes/ui.gd
@@ -1,13 +1,10 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$SelectionBackground.rect_position = $BuildSelection.rect_position
+	$SelectionBackground.rect_size = $BuildSelection.rect_size
+	$SelectionBackground.rect_scale = $BuildSelection.rect_scale
 	pass # Replace with function body.
 
 
@@ -15,9 +12,29 @@ func _ready():
 #func _process(delta):
 #	pass
 
+func _on_PlantsButton_pressed():
+	Statistics.selected_type = "plant"
+	$BuildSelection/PlantsButton.pressed = true
+	$BuildSelection/HouseButton.pressed = false
+	$BuildSelection/SolarButtton.pressed = false
+	#$SelectionHighlight.rect_position = $BuildSelection/PlantsButton.rect_position
+	#$SelectionHighlight.rect_size = $BuildSelection/PlantsButton.rect_size
+	#$SelectionHighlight.rect_scale = $BuildSelection/PlantsButton.rect_scale
 
-func _on_CheckBox_toggled(button_pressed):
-	if Statistics.selected_type == "plant":
-		Statistics.selected_type = "house"
-	else:
-		Statistics.selected_type = "plant"
+func _on_HouseButton_pressed():
+	Statistics.selected_type = "house"
+	$BuildSelection/PlantsButton.pressed = false
+	$BuildSelection/HouseButton.pressed = true
+	$BuildSelection/SolarButtton.pressed = false
+	#$SelectionHighlight.rect_position = $BuildSelection/HouseButton.rect_position
+	#$SelectionHighlight.rect_size = $BuildSelection/HouseButton.rect_size
+	#$SelectionHighlight.rect_scale = $BuildSelection/HouseButton.rect_scale
+
+
+func _on_SolarButtton_pressed():
+	Statistics.selected_type = "solor"
+	$BuildSelection/PlantsButton.pressed = false
+	$BuildSelection/HouseButton.pressed = false
+	$BuildSelection/SolarButtton.pressed = true
+	#$SelectionHighlight.rect_size = $BuildSelection/SolarButton.rect_size
+	#$SelectionHighlight.rect_scale = $BuildSelection/SolarButton.rect_scale
