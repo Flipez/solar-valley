3c2f8dd470473ed6e2b76707ac45960d9edcbb79
skib_ju <juliane.skibbe@dlr.de>
2021-10-09 23:10:30 +0200

    added plants instead of sheeps

--- Scenes/grass.gd
+++ Scenes/grass.gd
@@ -5,7 +5,7 @@ var type = "grass"
 
 func _ready():
 	$tmpParent/building_house.visible = false
-	$tmpParent/building_sheep.visible = false
+	$tmpParent/building_plant_1.visible = false
 	$tmpParent/grass.visible = true
 	pass
 	
@@ -21,10 +21,10 @@ func _on_grass_mouse_entered():
 	highlight_material.emission = Color(125, 125, 125)
 	get_children()[0].get_children()[0].set_material_override(highlight_material)
 	if Statistics.selected_type == "plant":
-		$tmpParent/building_sheep.visible = true	
+		$tmpParent/building_plant_1.visible = true	
 		$tmpParent/building_house.visible = false
 	elif Statistics.selected_type == "house":
-		$tmpParent/building_sheep.visible = false	
+		$tmpParent/building_plant_1.visible = false	
 		$tmpParent/building_house.visible = true
 	$tmpParent/grass.visible = false
 	is_hovered = true
@@ -33,7 +33,7 @@ func _on_grass_mouse_entered():
 func _on_grass_mouse_exited():
 	get_children()[0].get_children()[0].set_material_override(null)
 	$tmpParent/building_house.visible = false
-	$tmpParent/building_sheep.visible = false
+	$tmpParent/building_plant_1.visible = false
 	$tmpParent/grass.visible = true
 	is_hovered = false
 
@@ -42,7 +42,7 @@ func _input(event):
 		if event.is_pressed() and event.button_index == BUTTON_LEFT and is_hovered:
 			var hex
 			if Statistics.selected_type == "plant":
-				hex = load("res://Scenes/building_sheep.tscn").instance()
+				hex = load("res://Scenes/building_plant_1.tscn").instance()
 			elif Statistics.selected_type == "house":
 				hex = load("res://Scenes/building_house.tscn").instance()
 				hex.connect("more_people", Statistics, "more_people")
