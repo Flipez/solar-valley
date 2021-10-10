88953656f4e10c329ba5b7bfe0a54c55ba411c88
Sebastian Kümper <skuemper@gmx.de>
2021-10-10 18:01:10 +0200

    add well button

--- Scenes/ui.gd
+++ Scenes/ui.gd
@@ -9,15 +9,25 @@ func _on_PlantsButton_pressed():
   $ButtonBackground/BuildSelection/PlantsButton.pressed = true
   $ButtonBackground/BuildSelection/HouseButton.pressed = false
   $ButtonBackground/BuildSelection/SolarButtton.pressed = false
+  $ButtonBackground/BuildSelection/WellButton.pressed = false
 
 func _on_HouseButton_pressed():
   Statistics.selected_type = "house"
   $ButtonBackground/BuildSelection/PlantsButton.pressed = false
   $ButtonBackground/BuildSelection/HouseButton.pressed = true
   $ButtonBackground/BuildSelection/SolarButtton.pressed = false
+  $ButtonBackground/BuildSelection/WellButton.pressed = false
 
 func _on_SolarButtton_pressed():
   Statistics.selected_type = "solar"
   $ButtonBackground/BuildSelection/PlantsButton.pressed = false
   $ButtonBackground/BuildSelection/HouseButton.pressed = false
   $ButtonBackground/BuildSelection/SolarButtton.pressed = true
+  $ButtonBackground/BuildSelection/WellButton.pressed = false
+
+func _on_WellButton_pressed():
+  Statistics.selected_type = "well"
+  $ButtonBackground/BuildSelection/PlantsButton.pressed = false
+  $ButtonBackground/BuildSelection/HouseButton.pressed = false
+  $ButtonBackground/BuildSelection/SolarButtton.pressed = false
+  $ButtonBackground/BuildSelection/WellButton.pressed = true
