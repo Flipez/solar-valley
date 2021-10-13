4467d8c34ba2a8fa19be1fdf9d64bc1a0621e137
Flipez <code@brauser.io>
2021-10-13 15:12:37 +0200

    fix surrounding paths and placeholder

--- Scenes/Hexagons/house.gd
+++ Scenes/Hexagons/house.gd
@@ -10,7 +10,7 @@ signal update_people
 onready var influence_range = $influence_range
 onready var label           = $Spatial
 onready var label_text      = $Spatial/Viewport/Label
-onready var smoke           = $tmpParent/Smoke
+onready var smoke           = $tmpParent/building_house/Smoke
 
 
 func _ready():
