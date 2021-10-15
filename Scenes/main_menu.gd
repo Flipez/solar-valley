e87c0f4fb4dd97b1e191b8f85a7a4ca1877d115f
Flipez <code@brauser.io>
2021-10-15 21:10:44 +0200

    Revert "added tutorial"
    This reverts commit b76a057adf0cad450e464196e87f8df7694bf397.

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -11,7 +11,7 @@ func _process(_delta):
 
 
 func _on_new_pressed():
-    get_tree().change_scene("res://Scenes/main.tscn")
+    get_tree().change_scene("res://Scenes/menu_control.tscn")
 
 
 func _on_load_game_pressed():
@@ -19,7 +19,7 @@ func _on_load_game_pressed():
 
 
 func _on_tutorial_pressed():
-  get_tree().change_scene("res://Scenes/menu_tutorial.tscn")
+  pass # Replace with function body.
 
 
 func _on_controls_pressed():
